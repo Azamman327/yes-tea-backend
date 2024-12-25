e0eb95ce19762f405eeaf73558a3a654d1f3c764
Azamman327 <wjsdbdud327@naver.com>
2024-12-25 14:13:27 +0900

    Feat: Inventory domain 작성

--- src/main/java/com/example/tea_backend/domain/Inventory.java
+++ src/main/java/com/example/tea_backend/domain/Inventory.java
@@ -1,4 +1,19 @@
 package com.example.tea_backend.domain;
 
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
 public class Inventory {
+    private Long teaId;
+    private Long userId;
+    private int amount;
+    private int year;   //expired
+    private int month;  //expired
+    private int day;    //expired
 }
