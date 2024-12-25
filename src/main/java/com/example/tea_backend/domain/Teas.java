a69e99fd3072d9418e0aff107f6491d25af53344
Azamman327 <wjsdbdud327@naver.com>
2024-12-25 14:10:13 +0900

    Feat: Teas domain 작성

--- src/main/java/com/example/tea_backend/domain/Teas.java
+++ src/main/java/com/example/tea_backend/domain/Teas.java
@@ -1,4 +1,27 @@
 package com.example.tea_backend.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Teas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long teaId;
+    private String brand;
+    private String name;
+    private String packagingtype;
+    private String type;
+    private int minute;
+    private int second;
+    private float temperature;
+    private float quantity;
+    private int watervolume;
 }
